5e27a5aa98080f98c1140ce142d8aa115e951244
Vishal Yadav <yadavvi91@gmail.com>
2023-11-13 23:29:58 +0530

    This doesn't work

--- src/main/java/org/yadavvi/taskscheduler/config/SecurityConfig.java
+++ src/main/java/org/yadavvi/taskscheduler/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package org.yadavvi.taskscheduler.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -42,4 +44,11 @@ public class SecurityConfig {
                 );
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        return auth.build();
+    }
+
 }
