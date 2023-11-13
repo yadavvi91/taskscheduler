b3eac89eeaa98e53552e24950153a1847399ab06
Vishal Yadav <yadavvi91@gmail.com>
2023-11-13 13:13:00 +0530

    Starting to add SecurityConfig and Exception Handler

---
+++ src/main/java/org/yadavvi/taskscheduler/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package org.yadavvi.taskscheduler.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
--- src/main/java/org/yadavvi/taskscheduler/service/AuthenticationService.java
+++ src/main/java/org/yadavvi/taskscheduler/service/AuthenticationService.java
@@ -1,5 +1,8 @@
 package org.yadavvi.taskscheduler.service;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.yadavvi.taskscheduler.dto.request.JwtTokenRequest;
 import org.yadavvi.taskscheduler.dto.request.SignupRequest;
@@ -9,11 +12,16 @@ import org.yadavvi.taskscheduler.dto.response.SignupResponse;
 @Service
 public class AuthenticationService {
 
+    private AuthenticationManager authenticationManager;
+
     public SignupResponse signup(SignupRequest request) {
         return null;
     }
 
     public JwtTokenResponse login(JwtTokenRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+
         return null;
     }
 }
